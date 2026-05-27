6986184abd3e51bee63ca3f0f504579a1aa06c90
Akai <161771111+Akaiokaa@users.noreply.github.com>
2026-05-27 16:25:52 -0700

    Added a test case for addValue method

--- src/HeapTest.java
+++ src/HeapTest.java
@@ -3,7 +3,35 @@ import org.junit.Test;
 
 public class HeapTest {
     @Test
-    public void testIsEmpty() {
+    public void testEmptyHeap(){
+
+    }
+    
+    @Test
+    public void testHeapOrder() {
+        /*
+              1
+             / \
+            2   2
+           / \
+          3   4 
+        */
+        //Arrange
+        Heap heap = new Heap();
+        int[] order = new int[]{1,2,2,3,4};
+        heap.addValue(1);
+        heap.addValue(2);
+        heap.addValue(2);
+        heap.addValue(3);
+        heap.addValue(4);
         
+        for (int i = 0; i < heap.list.size(); i++) {
+            //Act
+            int Actual = heap.list.get(i);
+
+            //assert
+            assertEquals(Actual, order[i]);
+        }
+      
     }
 }
