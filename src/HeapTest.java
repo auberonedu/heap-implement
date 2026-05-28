28f04016ef2a927e1769a243482a2ef8b922c4bd
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2026-05-28 04:34:14 -0700

    add test

--- src/HeapTest.java
+++ src/HeapTest.java
@@ -3,4 +3,17 @@ import org.junit.Test;
 
 public class HeapTest {
 
+    @Test
+    public void testAdd() {
+        Heap heap = new Heap();
+
+        heap.add(10);
+        heap.add(5);
+        heap.add(2);
+        heap.add(6);
+        heap.add(20);
+        heap.add(1);
+
+        assertEquals(1, heap.peek());
+    }
 }
