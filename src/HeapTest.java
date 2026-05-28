74d3b289b46c8f8a09c27990b3be0f613cef4378
Lilian <210286008+liliann19@users.noreply.github.com>
2026-05-28 11:08:16 -0700

    Added more tests

--- src/HeapTest.java
+++ src/HeapTest.java
@@ -58,4 +58,61 @@ public class HeapTest {
         assertEquals(7, heap.pop());
         assertEquals(7, heap.pop());
     }
+
+    @Test
+    public void testNegativeValues() {
+        Heap heap = new Heap();
+
+        heap.add(-4);
+        heap.add(-9);
+        heap.add(0);
+        heap.add(1);
+
+        assertEquals(-9, heap.pop());
+        assertEquals(-4, heap.pop());
+        assertEquals(0, heap.pop());
+        assertEquals(1, heap.pop());
+    }
+
+    @Test 
+    public void testPeek() {
+        Heap heap = new Heap();
+
+        heap.add(5);
+        heap.add(7);
+        heap.add(2);
+
+        assertEquals(2, heap.peek());
+    }
+
+    @Test
+    public void testSize() {
+        Heap heap = new Heap();
+
+        heap.add(1);
+        heap.add(3);
+        heap.add(5);
+        heap.add(7);
+        heap.add(9);
+        heap.add(11);
+
+        assertEquals(6, heap.size());
+    }
+
+    @Test
+    public void testSizeSingleValue() {
+        Heap heap = new Heap();
+
+        heap.add(0);
+
+        assertEquals(1, heap.size());
+    }
+
+    @Test
+    public void testHeapIsEmpty() {
+        Heap heap = new Heap();
+
+        assertEquals(0, heap.size());
+        assertTrue(heap.isEmpty());
+    }
 }
