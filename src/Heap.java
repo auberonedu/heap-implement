4c8d49dab0867fde35bf7510ef77dc17131e361a
Azeb Tesfay <azbseb@gmail.com>
2026-05-30 07:05:27 -0700

    added the functions to get the basic

--- src/Heap.java
+++ src/Heap.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
+
 /**
- * A min-heap. 
+ * A min-heap.
  * 
  * Must contain functionality for the following: (n = # of elements in heap)
  * 
- * Adding a new value to the heap  O(log(n))
+ * Adding a new value to the heap O(log(n))
  * Popping the front of the heap O(log(n))
  * Peeking at the front of the heap without removing the element O(1)
  * Getting the size of the heap O(1)
@@ -12,15 +14,54 @@
  * 
  * The Heap must hold ints
  * 
- * Use List/ArrayList as a backing array, but DO NOT use the 
+ * Use List/ArrayList as a backing array, but DO NOT use the
  * PriorityQueue class. Do not make Node objects, use array
  * indexing instead.
  * 
  * It is up to you to make design decisions about how to:
- *  - name methods
- *  - determine return value and argument types
- *  - hold private instance variables
+ * - name methods
+ * - determine return value and argument types
+ * - hold private instance variables
  */
 public class Heap {
+
+ private ArrayList<Integer> items = new ArrayList<>();
+ private int size;
+
+ private int getLeftChildIndex(int parentIndex) {
+  return 2 * parentIndex + 1;
+ }
+
+ private int getRightChildIndex(int parentIndex) {
+  return 2 * parentIndex + 1;
+ }
+
+ private int getParentIndex(int childIndex) {
+  return (childIndex - 1) / 2;
+ }
+
+ private boolean hasLeftChild(int index) {
+  return getLeftChildIndex(index) < size;
+ }
+
+ private boolean hasRightChild(int index) {
+  return getRightChildIndex(index) < size;
+ }
+
+ private boolean hasParent(int index) {
+  return getParentIndex(index) >= 0;
+ }
+
+ private int leftChild(int index) {
+  return items.get(getLeftChildIndex(index));
+ }
+
+ private int rightChild(int index) {
+  return items.get(getRightChildIndex(index));
+ }
+
+ private int parent(int index) {
+  return items.get(getParentIndex(index));
+ }
 
 }
